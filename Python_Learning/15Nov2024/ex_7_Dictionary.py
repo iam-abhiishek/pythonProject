0434cf262268ae3e4d6dccfb0a3feee58c285874
abhishek <abhiishek.kr@gmail.com>
2024-11-15 23:28:05 +0530

    changes in code 7

--- Python_Learning/15Nov2024/ex_7_Dictionary.py
+++ Python_Learning/15Nov2024/ex_7_Dictionary.py
@@ -1,8 +1,10 @@
 # data = {}
 # print(type(data))
+from os import PRIO_PGRP
 
 data1 = {'JS':'Atom', 'Python':['Pycharm','Sublime'], 'Java':{'JSE':'Netbeans','JEE':'Eclipse'}}
 print(data1['Python'])
 print(data1['Python'][1])
 print(data1.get('Java'))
 print(data1['Java']['JEE'])
+print(data1)
